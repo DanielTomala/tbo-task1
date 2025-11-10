fc3efe67df8ea21eedf14f6e300df20c6fc7d739
DanielTomala <danieltomala01@gmail.com>
2025-11-10 11:45:49 +0100

    Prevent XSS in form by adding matchesPattern methods

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/mapper/StudentMapper.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/mapper/StudentMapper.java
@@ -1,31 +1,50 @@
 package com.example.thymeleaf.dto.mapper;
 
+import static org.apache.commons.lang3.Validate.matchesPattern;
+
 import com.example.thymeleaf.dto.CreateStudentDTO;
 import com.example.thymeleaf.dto.StudentResponseDTO;
 import com.example.thymeleaf.entity.Address;
 import com.example.thymeleaf.entity.Student;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StudentMapper {
 
+    private static final String LETTERS = "[A-Za-z-\\s]+";
+    private static final String LETTERS_AND_NUMBERS = "[0-9A-Za-z-\\s]+";
+
     public static Student toEntity(CreateStudentDTO dto) {
         Student student = new Student();
+
+        matchesPattern(dto.getName(), LETTERS, "Invalid name");
         student.setName(dto.getName());
+
         student.setEmail(dto.getEmail());
         student.setBirthday(dto.getBirthday());
 
         Address address = new Address();
         address.setZipCode(dto.getZipCode());
+
+        matchesPattern(dto.getStreet(), LETTERS, "Invalid street");
         address.setStreet(dto.getStreet());
+
+        matchesPattern(dto.getNumber(), LETTERS_AND_NUMBERS, "Invalid number");
         address.setNumber(dto.getNumber());
+
+        matchesPattern(dto.getComplement(), LETTERS, "Invalid complement");
         address.setComplement(dto.getComplement());
+
+        matchesPattern(dto.getDistrict(), LETTERS, "Invalid district");
         address.setDistrict(dto.getDistrict());
+
+        matchesPattern(dto.getCity(), LETTERS, "Invalid city");
         address.setCity(dto.getCity());
+
+        matchesPattern(dto.getCity(), LETTERS, "Invalid state");
         address.setState(dto.getState());
         address.setStudent(student);
 
